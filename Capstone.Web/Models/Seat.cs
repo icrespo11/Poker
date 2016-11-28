fbf865a982593dca78597e699b1a26f64f3e1882
Isaac Crespo <isaac.crespo11@gmail.com>
2016-11-28 14:48:03 -0500

    Some poker rules

---
+++ Capstone.Web/Models/Seat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class Seat
+    {
+        public int SeatNumber { get; set; }
+
+        public HoldEmHand Hand;
+
+        public bool Occupied { get; set; }
+
+
+        //this stuff might be a player class
+        public int TableBalance { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
---
+++ Capstone.Web/Models/Table.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class Table
+    {
+        public List<Seat> Seats;
+
+        public int Pot { get; set; }
+
+        //Or use Community card class?
+        public List<Card> CommunityCards;
+
+        public int MaxBet { get; set; }
+
+        public int MinBet { get; set; }
+
+        public int SmallBlind { get; set; }
+
+        public int BigBlind { get; set; }
+
+        public int Ante { get; set; }
+    }
+}
