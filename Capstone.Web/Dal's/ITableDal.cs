5f58df15f240d32ed50578e067930ac8fa10b3ef
Daniel Shier <shierd65@gmail.com>
2016-11-30 14:06:15 -0500

    table dal interface

---
+++ Capstone.Web/Dal's/ITableDal.cs
@@ -0,0 +1,16 @@
+﻿using Capstone.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.Dal_s
+{
+    interface ITableDal
+    {
+        Table FindTable(int tableID);
+        bool CreateTable(Table table, UserModel user);
+        List<Table> GetAllTables();
+    }
+}
