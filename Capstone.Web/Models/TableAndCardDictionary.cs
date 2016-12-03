accad289f147c720d2e5c009f4450b2c0cfa68cf
Bob Pollack <rap10@case.edu>
2016-12-03 15:55:57 -0500

    added a new model, about to break stuff

---
+++ Capstone.Web/Models/TableAndCardDictionary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class TableAndCardDictionary
+    {
+        public Table Table { get; set; }
+        public Dictionary<Card, bool> CardList { get; set; }
+        //possibly add something for other players' cards.
+    }
+}
